9964f23ca158f826b011396efa32e4e7a5278778
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2020-11-20 11:02:43 +0100

    test push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <footer>
-      <p>Made by Claudia Kjerrgren, Sabina van der Schaaf de Maré, Claire Caudwell, Vanessa Sue Smith</p>
+      <p>Test</p>
       <p>Technigo Botocamp Fall 2020</p>
     </footer>
   )
